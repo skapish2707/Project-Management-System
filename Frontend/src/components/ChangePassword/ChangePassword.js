9ba2d07f4a8342838ee0fbd81e82f2d53f312a1d
Abhishek Vaish <abhishek953566936222@gmail.com>
2020-08-27 19:55:21 +0530

    Updated changepassword page

--- Frontend/src/components/ChangePassword/ChangePassword.js
+++ Frontend/src/components/ChangePassword/ChangePassword.js
@@ -3,16 +3,84 @@ import axios from "axios";
 import "./ChangePassword.css";
 import SERVER_URL from "../../Pages/URL";
 import qs from "qs";
+import { LinearProgress } from "@material-ui/core";
+import {Redirect} from "react-router-dom";
+import LoggedNavbar from "../Navbar/LoggedNavbar";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Button from "@material-ui/core/Button";
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    [theme.breakpoints.up("md")]: {
+      width: "100%"
+    },
+    margin: "auto",
+    alignItems: "center"
+  },
+
+  form: {
+    width: "90%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  fields: {
+    backgroundColor: "#fff"
+  },
+  Input: {
+    border: "1px solid #d1d1d1",
+    padding: "4px 5px",
+    width: "55%",
+    marginBottom: "20px",
+    marginTop: "15px",
+    float: "right",
+    borderRadius: "4px"
+  },
+  deptLabel : {
+    marginTop: "25px",
+    marginBottom: "15px",
+    float : "left",
+  }
+});
 class ChangePassword extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      user : "",
       newPassword: "",
-      confirmPassword: ""
+      confirmPassword: "",
+      openSuccess : "",
     };
   }
+
+  getStat = () => {
+    axios({
+      method: "get",
+      url: SERVER_URL + "/user",
+      withCredentials: true
+    }).then(res => {
+        this.setState({user: res.data,});
+      }).catch(err => {
+        this.setState({user:'no user'})
+        localStorage.removeItem("token");
+      });
+  };
 
   pushPassword = () => {
     axios({
@@ -27,73 +95,125 @@ class ChangePassword extends Component {
       headers: {
         "content-type": "application/x-www-form-urlencoded;charset=utf-8"
       }
-    })
-      .then(function (res) {
-        console.log(res);
-      })
-
-      .catch(function (err) {
+    }).then(res => {
+        this.setState({openSuccess:true});
+      }).catch(function (err) {
         console.log(err);
       });
   };
 
-  newpasswordHandler = e => {
-    this.setState({ newPassword: e.target.value });
-  };
-
-  confirmpasswordHandler = e => {
-    this.setState({ confirmPassword: e.target.value });
-  };
+  newpasswordHandler = e => {this.setState({ newPassword: e.target.value });};
+  confirmpasswordHandler = e => {this.setState({ confirmPassword: e.target.value });};
 
   handleSubmit = e => {
     e.preventDefault();
-    if (this.state.newpassword !== this.state.confirmpassword) {
-      console.log(this.state);
-      alert("Please enter same password in both fields");
-      this.setState({ newPassword: "", confirmPassword: "" });
-    } else {
-      console.log(this.state);
-      alert("Both fields are same");
+
+    if (this.state.newPassword !== this.state.confirmPassword || this.state.newPassword.length < 8) {
+      this.setState({
+        newPassword : "",
+        confirmPassword : "",
+      });
+      console.log(this.state)
+    }
+    else {
       this.pushPassword();
     }
   };
 
   render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="changepassword-form">
-        <div className="changepassword-title">
-          <label>Change Password</label>
-        </div>
-        <label className="changepassword-label">New Password : </label>
-        <br />
-        <br />
-        <input
-          className="changepassword-input"
-          type="password"
-          name="password"
-          value={this.state.newPassword}
-          onChange={this.newpasswordHandler}
-          required
-        />
-        <br />
-        <br />
-        <label className="changepassword-label">Confirm Password : </label>
-        <br />
-        <br />
-        <input
-          className="changepassword-input"
-          type="password"
-          name="password"
-          value={this.state.confirmPassword}
-          onChange={this.confirmpasswordHandler}
-          required
-        />
-        <br />
-        <br />
-        <button className="changepassword-btn">Confirm</button>
-      </form>
-    );
+    const { classes } = this.props;
+    const handleClose = (event, reason) => {
+      this.setState({openSuccess:false,user:'no user'})
+      localStorage.removeItem("token");
+    };
+    if(this.state.user === ""){
+      this.getStat();
+      return <LinearProgress />;
+    }else if(this.state.user === 'no user'){
+      return <Redirect to="/" />;
+    }else {
+        if (this.state.newPassword !== this.state.confirmPassword){
+          var helptext = "Two fields Doesn't match"
+        }
+        else if (this.state.newPassword === this.state.confirmPassword) {
+          var helptext ="";
+          if (this.state.newPassword && this.state.newPassword.length < 8 ){
+            var helptext = "Please Enter a Password with length greater than 8";
+          }
+        }
+      return (
+        <div>
+          <LoggedNavbar />
+          <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div
+            className={classes.paper}
+            style={{
+              boxShadow:
+                "0 2px 4px rgba(0, 0, 0, .1), 0 8px 16px rgba(0, 0, 0, .1)",
+              backgroundColor: "#fff",
+              borderRadius: "6px"
+            }}
+          >
+           <Snackbar open={this.state.openSuccess}  onClose={handleClose} >
+              <Alert onClose={handleClose} severity="success">
+                Your Password was changed successfully Please click anywhere to login again
+              </Alert>
+            </Snackbar>
+            <form
+              className={classes.form}
+              onSubmit={this.handleSubmit}
+              noValidate
+            >
+              <Typography variant="h4" style={{ marginTop:'15px',marginBottom:'15px'}}>
+                Change Password
+              </Typography>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                type="password"
+                required
+                fullWidth
+                label="New Password"
+                value={this.state.newPassword}
+                onChange={this.newpasswordHandler}
+                autoFocus/>
+              <TextField
+                variant="outlined"
+                type="password"
+                margin="normal"
+                required
+                fullWidth
+                label="Confirm Password"
+                value={this.state.confirmPassword}
+                onChange={this.confirmpasswordHandler}
+                />
+                <Typography variant="subtitle" color="error">
+                  {helptext}
+                </Typography>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                style={{
+                  padding: "10px",
+                  fontSize: "18px",
+                  fontWeight: "bolder",
+                  backgroundColor: "#1877f2",
+                  marginBottom: "25px"
+                }}
+              >
+                Change Password
+              </Button>
+            </form>
+          </div>
+        </Container>
+      </div>        
+      );
+    }
   }
 }
 
-export default ChangePassword;
+export default withStyles(useStyles)(ChangePassword);
