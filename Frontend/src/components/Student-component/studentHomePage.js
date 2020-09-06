cde5f5fcedaafa856a7d79a68f122c91934e0fab
RitvijVejare <rutu9295@gmail.com>
2020-09-06 13:54:54 +0530

    added new StudentHomePage

---
+++ Frontend/src/components/Student-component/studentHomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme)=>{
+
+})
+
+const StudentHomePage = () => {
+    const classes = useStyles();
+    return ( 
+        <React.Fragment>
+            <Typography>YO</Typography>
+        </React.Fragment>
+     );
+}
+ 
+export default StudentHomePage;
