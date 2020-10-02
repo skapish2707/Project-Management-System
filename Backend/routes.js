add7217113eb88348e68fee05a0cd81da92af518
Abhishek Vaish <abhishek953566936222@gmail.com>
2020-10-02 17:56:07 +0530

    added route to get list of project guides

--- Backend/routes.js
+++ Backend/routes.js
@@ -76,7 +76,7 @@ router.post('/yami',function(req,res){
 	email = req.body.email;
 	department = req.body.department;
 	dbm.addToDatabase(null,name,null,email,department,"admin");
-	res.status(200).send("Done") ;
+	res.status(200).send("OK") ;
 });
 
 
@@ -245,8 +245,18 @@ router.post('/addGuide',async function(req,res){
 			await dbm.addToDatabase(req.user,req.body.name.trim(),null,req.body.email.trim(),req.user.department,'guide');
 		}
 		await dbm.addGuide(req.body.email.trim(),req.body.name.trim(),req.body.groupId.trim())
-		return res.status(200).send("OK")
+		return res.status(200).send("OK");
 	}catch {
+		return res.status(500).send();
+	}
+})
+router.get('/getGuide',async function(req,res){
+	if (!req.user) return res.status(404).send();
+	if (req.user.type != 'admin') return res.status(404).send();
+	try {
+		guides = await dbm.getGuide(req.user);
+		return res.status(200).send(guides);
+	}catch{
 		return res.status(500).send();
 	}
 })
