699306607806eca5e7a014cdc99f2d135939e47c
Abhishek Vaish <abhishek953566936222@gmail.com>
2020-11-03 21:53:14 +0530

    deploy #11

---
+++ Frontend/build/precache-manifest.927f0ba856ab5123307050a55943db55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2784d065438657c4f54afdb060c16473",
+    "url": "/index.html"
+  },
+  {
+    "revision": "132ea00aab2b6361afec",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "abf75dd623b20e54ba9e",
+    "url": "/static/css/main.48071b94.chunk.css"
+  },
+  {
+    "revision": "132ea00aab2b6361afec",
+    "url": "/static/js/2.211cd560.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.211cd560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf75dd623b20e54ba9e",
+    "url": "/static/js/main.aef0ef9c.chunk.js"
+  },
+  {
+    "revision": "fb7b973d3f2fab5ffa08",
+    "url": "/static/js/runtime-main.b88e30f6.js"
+  },
+  {
+    "revision": "43a914dcfdc6b409ef3203838b5f873d",
+    "url": "/static/media/background2.43a914dc.jpg"
+  }
+]);
