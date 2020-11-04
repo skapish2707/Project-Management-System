b66fa5e0e7ceb8cc471034131461b2aa15a20a8c
Abhishek Vaish <abhishek953566936222@gmail.com>
2020-11-04 10:22:02 +0530

    deploy #16

--- Frontend/src/Pages/URL.js
+++ Frontend/src/Pages/URL.js
@@ -2,4 +2,5 @@ if (process.env.NODE_ENV == "production")
 	var SERVER_URL = "https://project-management-system-ark.herokuapp.com:"+process.env.PORT;
 else	
 	var SERVER_URL = "http://127.0.0.1:8000";
+console.log(process.env.NODE_ENV);
 export default SERVER_URL;
