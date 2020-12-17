cad38dbb707dde11fe704ba5da195bd8761dea1c
Abhishek Vaish <abhishek953566936222@gmail.com>
2020-12-17 10:28:45 +0530

    build

--- Frontend/build/precache-manifest.5ce1422c3c4b01d75f0e069753defaa6.js
+++ Frontend/build/precache-manifest.5ce1422c3c4b01d75f0e069753defaa6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1b9ca2b8ec226965aa770c8b6b0d4215",
+    "revision": "384a038795c722bf0aad3e203cda7a88",
     "url": "/index.html"
   },
   {
-    "revision": "24a11f30ee7a63d48135",
+    "revision": "045093a8c28305bc81a4",
     "url": "/static/css/2.5c17b9f1.chunk.css"
   },
   {
-    "revision": "590cccc2ead29ae19c14",
+    "revision": "7c87fd96a01a7538dfb4",
     "url": "/static/css/main.647a13eb.chunk.css"
   },
   {
-    "revision": "24a11f30ee7a63d48135",
-    "url": "/static/js/2.fc50bd94.chunk.js"
+    "revision": "045093a8c28305bc81a4",
+    "url": "/static/js/2.a58a1ff5.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.fc50bd94.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a58a1ff5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "590cccc2ead29ae19c14",
-    "url": "/static/js/main.e333a09d.chunk.js"
+    "revision": "7c87fd96a01a7538dfb4",
+    "url": "/static/js/main.d275be7f.chunk.js"
   },
   {
     "revision": "fb7b973d3f2fab5ffa08",
@@ -30,6 +30,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "43a914dcfdc6b409ef3203838b5f873d",
     "url": "/static/media/background2.43a914dc.jpg"
+  },
+  {
+    "revision": "8670826915470e6b28e4ad681b1cb7ff",
+    "url": "/static/media/kj1.86708269.png"
+  },
+  {
+    "revision": "51657a9c7ff17ef569828380812db84f",
+    "url": "/static/media/kjsieit-navbar.51657a9c.png"
   },
   {
     "revision": "fcf683217b98a5d7ac26fcfa614377a6",
--- Frontend/build/service-worker.js
+++ Frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd67ac20a221833afdf3b493dbc7d386.js"
+  "/precache-manifest.5ce1422c3c4b01d75f0e069753defaa6.js"
 );
 
 self.addEventListener('message', (event) => {
