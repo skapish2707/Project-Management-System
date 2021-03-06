23ab549ed54b337426eb5c1049d0de5e6a61dfb2
Abhishek Vaish <abhishek953566936222@gmail.com>
2021-03-06 13:04:10 +0530

    archive response

--- models/Archive.js
+++ models/Archive.js
@@ -4,36 +4,31 @@ var archiveSchema  = new mongoose.Schema({
 	admin : {type : mongoose.Schema.Types.ObjectId,ref : 'User'},
 	data : [{
 		acadYear : String,
-		groups  : [{
-			name:String,
-			members : [{
-				name : String,
-				email : String,
-				rollno : String,
-			}],
-			proposals : [{
-				title : String,
-				specialization : String,
-				details : String,
-				agency : String,
-				method : String,
-				result : String,
-				requirements : String,
-				attachPrints : String, 
-				approval : {
-					admin : { type :Boolean,default:false },
-					hod : { type :Boolean,default:false },
-				},
-				applied : {
-					type : Date,
-					default: Date.now 
-				},
-			}],
-			guide : {
-				name : {type:String,default:null},
-				email : {type:String,default:null},
-			},
-		}]	
+		members: [{
+			name: String,
+			email: String,
+		}],
+		project : {
+			title: { type: String, default: null },
+			specialization: { type: String, default: null },
+			details: { type: String, default: null },
+			agency: { type: String, default: null },
+			method: { type: String, default: null },
+			result: { type: String, default: null },
+			requirements: { type: String, default: null },
+			typeOfProject: { type: String, default: null },
+			category: { type: String, default: null },
+			attachPrints: { type: String, default: null },
+		},
+		addtionalDocuments: [{
+			docName: { type: String, default: null },
+			desc: { type: String, default: null },
+			doclink: { type: String, default: null }
+		}],
+		guide : {
+			name : {type:String,default:null},
+			email : {type:String,default:null},
+		},
 	}]
 })
 
