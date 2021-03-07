39c957f2368135f7912c6b30621b696824f02100
kapish27 <megacap27@gmail.com>
2021-03-07 17:44:27 +0530

    table in archive

--- Frontend/src/components/ArchiveTable.js
+++ Frontend/src/components/ArchiveTable.js
@@ -23,21 +23,6 @@ const useRowStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein, price) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      { date: '2020-01-05', customerId: '11091700', amount: 3 },
-      { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
-    ],
-  };
-}
-
 function Row(props) {
   const {row} = props;
   console.log(row)
@@ -97,14 +82,6 @@ function Row(props) {
     </React.Fragment>
   );
 }
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
 
 export default function ArchiveTable(props) {
   const {archData} = props
